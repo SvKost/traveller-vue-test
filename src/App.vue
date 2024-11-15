81d67478b49dfa28a5987c9f29a1f67c1bdd688b
Svitlana Kostohryz <142225792+SvKost@users.noreply.github.com>
2024-11-15 19:32:24 +0200

    modal created

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <script setup>
+import { ref } from 'vue'
 import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
-// import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
+import IModal from './components/IModal/IModal.vue'
+import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
 // import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 // import HomepageView from './views/HomepageView.vue'
+
+const isOpen = ref(true)
+
+const closeModal = () => {
+  isOpen.value = false
+}
+
+const openModal = () => {
+  isOpen.value = true
+}
 </script>
 
 <template>
+  <button @click="openModal">Open Modal</button>
   <!-- <HomepageView /> -->
   <!-- <div class="bg-white shadow-[0px_4px_31px_0px_rgba(44,44,44,0.10)] h-screen w-[400px]">
     <FavoritePlaces />
   </div> -->
 
-  <!-- <RegistrationForm @submit="console.log" /> -->
+  <RegistrationForm @submit="console.log" />
   <LoginForm @submit="console.log" />
+  <IModal v-if="isOpen" @close="closeModal">Modal</IModal>
 </template>
