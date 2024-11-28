947bce0af355382c8f7d4f6939b383020e5cc591
Svitlana Kostohryz <142225792+SvKost@users.noreply.github.com>
2024-11-28 15:40:25 +0200

    composables created

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,25 +1,22 @@
 <script setup>
-import { ref } from 'vue'
 import { loginUser } from '../components/api/user'
 import LoginForm from '../components/Auth/LoginForm/LoginForm.vue'
 import { useRouter } from 'vue-router'
+import { useMutation } from '../composables/useMutation'
 
 const router = useRouter()
-const isLoading = ref(false)
 
-const handleLoginUser = async (userData) => {
-  isLoading.value = true
-  try {
-    await loginUser(userData)
-    router.replace('/map')
-  } catch (error) {
-    console.error(error)
-  } finally {
-    isLoading.value = false
-  }
-}
+const {
+  isLoading,
+  error,
+  mutation: handleLoginUser
+} = useMutation({
+  mutationFn: loginUser,
+  onSuccess: () => router.replace('/map')
+})
 </script>
 
 <template>
   <LoginForm @submit="handleLoginUser" :is-loading="isLoading" />
+  <div v-if="error" class="text-red-500">{{ error.message }}</div>
 </template>
--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,25 +1,22 @@
 <script setup>
-import { ref } from 'vue'
 import { registerUser } from '../components/api/user'
 import RegistrationForm from '../components/Auth/RegistrationForm/RegistrationForm.vue'
 import { useRouter } from 'vue-router'
+import { useMutation } from '../composables/useMutation'
 
 const router = useRouter()
-const isLoading = ref(false)
 
-const handleRegisterUser = async (userData) => {
-  isLoading.value = true
-  try {
-    await registerUser(userData)
-    router.replace('/map')
-  } catch (error) {
-    console.error('error: ', error)
-  } finally {
-    isLoading.value = false
-  }
-}
+const {
+  isLoading,
+  error,
+  mutation: handleRegisterUser
+} = useMutation({
+  mutationFn: registerUser,
+  onSuccess: () => router.replace('/map')
+})
 </script>
 
 <template>
   <RegistrationForm @submit="handleRegisterUser" :is-loading="isLoading" />
+  <div v-if="error" class="text-red-500">{{ error.message }}</div>
 </template>
